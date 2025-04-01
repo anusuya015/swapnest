13ec6b8727aba6242481d8cdb0dc4b98765d2971
anusuya015 <142157144+anusuya015@users.noreply.github.com>
2025-04-01 19:39:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,14 @@ import orderRoutes from './routes/orderRoutes.js'
 dotenv.config();
 
 connectDB();
-
+const allowedOrigins = [
+  "http://localhost:3000", 
+  "https://swapnest-frontend.onrender.com"
+];
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:3000",   // Your React app's URL
+  origin: allowedOrigins,   // Your React app's URL
   methods: ["GET", "POST","PUT", "DELETE"],
   credentials: true
 }));
@@ -35,16 +38,17 @@ app.get("/api/config/cloudinarypreset", (req, res) => {
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running...");
-  });
-}
+//app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+//if (process.env.NODE_ENV === "production") {
+  //app.use(express.static(path.join(__dirname, "/frontend/build")));
+  //app.get("*", (req, res) =>
+    //res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  //);
+//} else {
+//  app.get("/", (req, res) => {
+  //  res.send("API is running...");
+  //});
+//}
 
 app.use((err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
